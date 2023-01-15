29ea597f6c4224001bdb21f60a5e25e747534afb
Tirth Nagar <tirthnagar.n1@gmail.com>
2023-01-15 10:42:05 -0500

    final commit (proof of concept)

--- deltahacks/src/App.js
+++ deltahacks/src/App.js
@@ -1,28 +1,40 @@
 import "./App.css";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+import profile from "./profile.png";
 
-function App() {
-  const app = initializeApp(
-    "deltahacks/src/deltahacks-project-45cfa-firebase-adminsdk-tm41x-65ad7340b2.json"
-  );
+function firebase() {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+  };
+  const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   console.log(db);
+  // const ref = doc(db, "Clients", "Client_1");
+  // const docSnap = getDoc(ref);
+
+  // if (docSnap.exists()) {
+  //   console.log("Document data:", docSnap.data());
+  // } else {
+  //   // doc.data() will be undefined in this case
+  //   console.log("No such document!");
+  // }
+}
+
+function App() {
+  firebase();
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello Username</p>
+        <img className="App-logo" src={profile} />
+        <p className="App-link">Number of Pills: 5</p>
       </header>
     </div>
   );
